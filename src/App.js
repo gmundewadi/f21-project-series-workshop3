a4953aeb4d04cc96ceddab5e3defb97f77159e9d
Gautam Mundewadi <gautammundewadi@gmail.com>
2021-10-19 23:18:38 -0700

    gm - image gallery

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import ImageGallery from 'react-image-gallery';
+import "react-image-gallery/styles/css/image-gallery.css";
+
+const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+];
 
 function App() {
   return (
     <div>
       <h1> An Empty React App for Workshop 3 </h1>
+      <ImageGallery items={images} />
     </div>
   );
 }
